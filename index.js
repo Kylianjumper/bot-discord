0309c1fa27ef44efdc60f6612b5d326be342bd70
Kylianjumper <45971957+Kylianjumper@users.noreply.github.com>
2018-12-18 16:51:28 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord - require("discord.js");
+
+var bot - new Discord.client();
+
+bot.on("ready", function)() {
+    bot.user.setGame("DTTEAM Bot,d!help");
+    console.log("Le bot a bien ete connecter");
+));
+
+bot.login("NTE5ODk5MTAwNTAyNjg3NzQ0.DvqTBg.6okdr3gN5OspUaoLpGWE_etu_GE")
